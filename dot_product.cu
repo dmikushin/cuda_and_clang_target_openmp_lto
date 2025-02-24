a21394e5380ebb8e4277a662cd0125a16f992076
Dmitry Mikushin <dmitry@kernelgen.org>
2025-02-24 09:28:13 -0500

    Adding a note about inlining

--- dot_product.cu
+++ dot_product.cu
@@ -1,3 +1,9 @@
+#if 0
+// Will not work:
+//
+// nvlink error   : Undefined reference to '_Z10dotProductPfS_iii' in 'matmul.nvcc.o' 
+inline __attribute__((always_inline))
+#endif
 __device__ float dotProduct(float *A, float *B, int row, int col, int N) {
     float sum = 0.0f;
     for(int k = 0; k < N; ++k) {
